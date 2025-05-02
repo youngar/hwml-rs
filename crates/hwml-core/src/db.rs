ee8b7cab4920e0f08fde5e9d3b58fc3289ebe972
Robert Young <rwy0717@gmail.com>
2025-05-02 18:02:10 -0400

    Add a core syntax type

---
+++ crates/hwml-core/src/db.rs
@@ -0,0 +1,33 @@
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct Level(pub usize);
+
+impl Level {
+    pub fn new(x: usize) -> Level {
+        Level(x)
+    }
+
+    pub fn to_index(self: Level, depth: usize) -> Index {
+        Index(depth - self.to_usize() - 1)
+    }
+
+    pub fn to_usize(self: Level) -> usize {
+        self.0
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct Index(pub usize);
+
+impl Index {
+    pub fn new(x: usize) -> Index {
+        Index(x)
+    }
+
+    pub fn to_level(self: Index, depth: usize) -> Level {
+        Level(depth - self.to_usize() - 1)
+    }
+
+    pub fn to_usize(self: Index) -> usize {
+        self.0
+    }
+}
