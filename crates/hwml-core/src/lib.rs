d1a33865d71b2f96080a2f7fd47dd3dd368e9cec
Robert Young <rwy0717@gmail.com>
2025-05-02 18:02:10 -0400

    Add a core syntax type

--- crates/hwml-core/src/lib.rs
+++ crates/hwml-core/src/lib.rs
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 pub mod db;
 pub mod declaration;
 pub mod syntax;
+||||||| parent of 658504b (Add a core syntax type)
+=======
+pub mod db;
+pub mod syntax;
+>>>>>>> 658504b (Add a core syntax type)
