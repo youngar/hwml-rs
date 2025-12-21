d6735aef46be94780408576795746fc0bc4be43b
Robert Young <rwy0717@gmail.com>
2025-12-20 22:38:09 -0500

    Add vec to prelude

--- crates/hwml-core/src/prelude.rs
+++ crates/hwml-core/src/prelude.rs
@@ -85,8 +85,75 @@ pub fn def_option<'db>(db: &'db Database, g: &mut GlobalEnv<'db>) {
     )
 }
 
+pub fn def_vec<'db>(db: &'db Database, g: &mut GlobalEnv<'db>) {
+    let nat_id = "Nat".into_with_db(db);
+    let vec_id = "Vec".into_with_db(db);
+    // |- vec (a : type) : nat -> type.
+    g.add_type_constructor(
+        vec_id,
+        TypeConstructorInfo {
+            arguments: [
+                Syntax::universe_rc(UniverseLevel::new(0)),
+                Syntax::type_constructor_rc(nat_id, vec![]),
+            ]
+            .into(),
+            num_parameters: 1,
+            level: UniverseLevel::new(0),
+        },
+    );
+    // a : type |- vnil : vec a 0
+    g.add_data_constructor(
+        "VNil".into_with_db(db),
+        DataConstructorInfo {
+            arguments: [].into(),
+            // vec a 0
+            ty: Syntax::type_constructor_rc(
+                vec_id,
+                [
+                    Syntax::variable_rc(Index(0)),
+                    Syntax::data_constructor_rc("Zero".into_with_db(db), [].into()),
+                ]
+                .into(),
+            ),
+        },
+    );
+    // a : type |- vcons (n : nat) (x : a) (xs : vec a n) : vec a (suc n)
+    g.add_data_constructor(
+        "VCons".into_with_db(db),
+        DataConstructorInfo {
+            arguments: [
+                Syntax::type_constructor_rc(nat_id, vec![]), // n : nat
+                Syntax::variable_rc(Index(1)),               // x : a
+                // xs : vec a n
+                Syntax::type_constructor_rc(
+                    vec_id,
+                    [
+                        Syntax::variable_rc(Index(2)), // a
+                        Syntax::variable_rc(Index(1)), // n
+                    ]
+                    .into(),
+                ),
+            ]
+            .into(),
+            // vec a (succ n)
+            ty: Syntax::type_constructor_rc(
+                vec_id,
+                [
+                    Syntax::variable_rc(Index(3)), // a
+                    Syntax::data_constructor_rc(
+                        "Succ".into_with_db(db),
+                        [Syntax::variable_rc(Index(2))].into(), // n
+                    ),
+                ]
+                .into(),
+            ),
+        },
+    );
+}
+
 pub fn load<'db>(db: &'db Database, g: &mut GlobalEnv<'db>) {
     def_bool(db, g);
     def_nat(db, g);
     def_option(db, g);
+    def_vec(db, g);
 }
